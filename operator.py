c8dc186bc004c54a4f5308f70258906a4935672d
Gideon <trunghaonguyen.98@gmail.com>
2021-02-08 21:16:37 +0700

    new pj

---
+++ operator.py
@@ -0,0 +1,18 @@
+def sum(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return float(a / b)
+
+def sub(a, b):
+    return a - b
+
+def sum(list: list):
+    s = 0
+    for a in list:
+        s = s + a
+    return s
+    
